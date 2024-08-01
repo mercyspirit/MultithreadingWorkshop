a945302a1eff3868b5a159327dab013b98b22e77
Arden Qiu <ziying.qiu@gmail.com>
2024-07-31 20:35:11 -0400

    examples

--- threading_demo/answers/examples.py
+++ threading_demo/answers/examples.py
@@ -1,11 +1,9 @@
 def exercise1():
-    t1 = threading.Thread(target=make_pants, args=("green",))
-    t2 = threading.Thread(target=make_dress, args=("blue",))
+    t1 = threading.Thread(target=make_pants, args=("green",10))
+    t2 = threading.Thread(target=make_dress, args=("blue",10))
 
     t1.start()
     t2.start()
-
-    print("middle function!")
 
     t1.join()
     t2.join()
