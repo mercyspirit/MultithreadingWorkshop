4045b79572e3f1983554e6fde8cbc82de9758aa7
cg-ghc <cosette.goldstein@gmail.com>
2024-10-09 21:32:12 -0400

    Update main.py in mutex_final_answer

--- mutex_demo/mutex_final_answer/main.py
+++ mutex_demo/mutex_final_answer/main.py
@@ -1,24 +1,23 @@
 import time
 import threading
 import concurrent.futures
-from request_service import get
-from json_service import write_to_file
 import re
+from lottery-data import getData
 
 survey_dictionary_count = {}
-global counter
 counter = 0
+even_odd_sum = 0
 lock = threading.Lock()
 
 def add_row_to_dictionary(winning_numbers):
     global counter
-    
+    global even_odd_sum
     # Perform non-critical operations outside of the lock
-    time.sleep(0.03)
+    time.sleep(0.06)
     numbers_list = winning_numbers.split()
     
     for item in numbers_list:
-        time.sleep(0.004)
+        time.sleep(0.008)
         
         # Lock only the critical section where shared data is modified
         with lock:
@@ -27,15 +26,20 @@ def add_row_to_dictionary(winning_numbers):
             else:
                 survey_dictionary_count[item] += 1
             counter += int(item)
+            
+            if int(item) % 2 > 0:
+                even_odd_sum += int(item)
+            else:
+                even_odd_sum -= int(item)
 
 if __name__ == '__main__':
     print("Program start")
     start = time.time()
 
-    body = get('https://data.ny.gov/api/views/d6yy-54nr/rows.json?accessType=DOWNLOAD')
-    data = body["data"]
+    # data sourced from 'https://data.ny.gov/api/views/d6yy-54nr/rows.json?accessType=DOWNLOAD', downloaded on 9/22/2024
+    data = getData()
     
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
         futures = []
         for row in data:
             futures.append(executor.submit(add_row_to_dictionary, row[9]))
@@ -48,3 +52,4 @@ if __name__ == '__main__':
     end = time.time()
     difference = end - start
     print(f"Time elapsed: {difference}")
+    print(even_odd_sum)
